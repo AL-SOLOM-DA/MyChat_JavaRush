ecc53378aa21e2fddad7346e4b6fa2e80a457bbc
Dmitrii <ecolog.comvgorle@mail.ru>
2021-10-14 00:18:46 +0300

    Add some method to Client

--- src/com/solomanin/client/Client.java
+++ src/com/solomanin/client/Client.java
@@ -12,6 +12,54 @@ public class Client {
     private volatile boolean clientConnected = false;
 
     public class SocketThread extends Thread{
+        protected void processIncomingMessage(String message){
+            ConsoleHelper.writeMessage(message);
+        }
+
+        protected void informAboutAddingNewUser(String userName){
+            ConsoleHelper.writeMessage("Участник с именем "+userName + " присоединился к чату.");
+        }
+
+        protected void  informAboutDeletingNewUser(String userName){
+            ConsoleHelper.writeMessage("Участник с именем "+userName + " покинул чат");
+        }
+
+        protected void notifyConnectionStatusChanged(boolean clientConnected){
+            Client.this.clientConnected = clientConnected;
+            synchronized (Client.this){
+                Client.this.notify();
+            }
+        }
+
+        protected void clientHandsshake() throws IOException, ClassNotFoundException{
+            while (true){
+                Message message = connection.receive();
+                if(message.getType() == MessageType.NAME_REQUEST) {
+                    String name = getUserName();
+                    connection.send(new Message(MessageType.USER_NAME, name));
+                }
+                else if(message.getType()==MessageType.NAME_ACCEPTED){
+                    notifyConnectionStatusChanged(true);
+                    return;
+                } else throw new IOException("Unexpected MessageType");
+            }
+        }
+
+        protected void clientMainLoop() throws IOException, ClassNotFoundException{
+            while (true){
+                Message message = connection.receive();
+                if(message.getType() == MessageType.TEXT) {
+                    processIncomingMessage(message.getData());
+                }
+                else if(message.getType()==MessageType.USER_ADDED){
+                    informAboutAddingNewUser(message.getData());
+                }
+                else if (message.getType()==MessageType.USER_REMOVED) {
+                    informAboutDeletingNewUser(message.getData());
+                }
+                else throw new IOException("Unexpected MessageType");
+            }
+        }
     }
 
     protected String getServerAddress(){
@@ -48,6 +96,35 @@ public class Client {
     }
 
     public void run(){
+        SocketThread socketThread = getSocketThread();
+        socketThread.setDaemon(true);
+        socketThread.start();
+        synchronized (this){
+            try {
+                this.wait();
+            } catch (InterruptedException e) {
+                ConsoleHelper.writeMessage("Произошла ошибка");
+                return;
+            }
+        }
+        if(clientConnected){
+            ConsoleHelper.writeMessage("Соединение установлено. Для выхода наберите команду 'exit'.");
+        } else {
+            ConsoleHelper.writeMessage("Произошла ошибка во время работы клиента.");
+        }
+        while (clientConnected) {
+            String text = ConsoleHelper.readString();
+            if(text.equals("exit")){
+                return;
+            }
+            if(shouldSendTextFromConsole()){
+                sendTextMessage(text);
+            }
+        }
+    }
 
+    public static void main(String[] args) {
+        Client client = new Client();
+        client.run();
     }
 }
